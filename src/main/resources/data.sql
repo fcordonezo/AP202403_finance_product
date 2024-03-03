a469f7a6866c96828df9a25a1384f77f38e0f2a3
Fabian Camilo Ordoñez <camilo.ordonezo@pragma.com.co>
2024-03-02 22:15:07 -0500

    feat: Se agrega data de prueba en BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('CAH', 'Cuenta de ahorros');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('TDB', 'Tarjeta débito');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('TDC', 'Tarjeta de crédito');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('SEG', 'Seguro');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('INV', 'Inversión');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('GIR', 'Giro');
+INSERT INTO `finance_service` (`code`, `description`) VALUES ('TAM', 'Tarjeta amparada');
